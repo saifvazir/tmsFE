b5c4d2bb91c538f36a89689dabae96997d217fca
rohit kawale <rohit.kawale02@gmail.com>
2016-12-06 19:31:19 +0200

    google and fb button

--- js/signup.js
+++ js/signup.js
@@ -4,15 +4,13 @@
         var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
 
-
-
-
-
     	$scope.register= function(data){
     			var payload={
-    				'Username':data.Username,
-    				'Email_id':data.Email_id,
-    				'Password':data.Password
+    				'Username':data.Username || "",
+    				'Email_id':data.Email_id || "",
+    				'Password':data.Password || "",
+                    'is_google':data.google || 0,
+                    'is_facebook':data.fb || 0
     			};   
             var request=$http({  
                         method: 'POST',  
